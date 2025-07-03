34cfa1a6beaccf58a674ee9f28e294e2a196e84a
Khusan Mirobidov <husanmirobidov@gmail.com>
2025-07-03 14:37:27 +0500

    fix: delete views

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ const path = require("path");
 
 const ROUTE = 5050;
 
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "views"));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "../", "public")));
