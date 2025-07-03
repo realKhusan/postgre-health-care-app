d3d37a8bea7b62827584302eecfbedc92debc9ef
Khusan Mirobidov <husanmirobidov@gmail.com>
2025-07-03 15:11:50 +0500

    fix db file

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,9 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "",
-  password: "",
-  database: "",
+  user: "postgres",
+  password: "1234567890",
+  database: "postgres",
   host: "localhost",
   port: 1234,
 });
